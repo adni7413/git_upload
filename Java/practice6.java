c87de1b7deb647655325674eadd3c3879f60badb
Roger_00550341 <adni0754686@gmail.com>
2024-08-14 14:45:23 +0800

    CR後更新

--- Java/practice6.java
+++ Java/practice6.java
@@ -14,116 +14,126 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class practice6 {
 
 	public static void main(String[] args) {
 
-		try {
-			InputStreamReader isr = new InputStreamReader(
-					new FileInputStream("C:\\Users\\Admin\\Downloads\\Java評量_第6題cars.csv"));// 檔案讀取路徑
-			BufferedReader reader = new BufferedReader(isr);
-			String line = null;
+		// 第 6-1 題
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream("C:\\Users\\Admin\\Downloads\\Java評量_第6題cars.csv")));) {
+			// 檔案讀取路徑
 
-			//建立list儲存車輛資料
+			String line;
+
+			// 讀取csv首列資料，並將四種屬性資料以Array儲存
+			String[] titleArray = reader.readLine().split(",");
+
+			// 建立set儲存車輛製造商資料
+			Set<String> manufactureSet = new TreeSet<String>();
+
+			// 建立list儲存車輛資料
 			List<Map<String, String>> carList = new ArrayList<>();
 
-			//讀取csv各行資料，並將四種屬性資料以Map傳入list
+			// 讀取csv各行資料，並將四種屬性資料以Map傳入list，同時將不重複的製造商名稱傳入set
 			while ((line = reader.readLine()) != null) {
 
 				Map<String, String> map = new HashMap<>();
 
 				String item[] = line.split(",");
 				/** 讀取 **/
-				String data1 = item[0].trim();
-				String data2 = item[1].trim();
-				String data3 = item[2].trim();
-				String data4 = item[3].trim();
-				map.put("MANUFACTURER", data1);
-				map.put("TYPE", data2);
-				map.put("MIN.PRICE", data3.toString());
-				map.put("PRICE", data4.toString());
 
+				map.put(titleArray[0], item[0].trim()); // trim()代表去除字串的前後空格
+				map.put(titleArray[1], item[1].trim());
+				map.put(titleArray[2], item[2].trim());
+				map.put(titleArray[3], item[3].trim());
+
+				manufactureSet.add(item[0].trim());
 				carList.add(map);
-				
+
 			}
 
-			//刪掉首列標題資料，方便後續比較價格順序
-			carList.remove(0);
-			//建立比較方法，先在compare方法內將字串轉成BigDecimal由大排到小比較價格
+			// 建立比較方法，先在compare方法內將字串轉成BigDecimal由大排到小比較價格
 			Collections.sort(carList, new Comparator<Map<String, String>>() {
 
 				@Override
 				public int compare(Map<String, String> o1, Map<String, String> o2) {
-					BigDecimal price1 = new BigDecimal(o1.get("PRICE"));
-					BigDecimal price2 = new BigDecimal(o2.get("PRICE"));
+					BigDecimal price1 = new BigDecimal(o1.get(titleArray[3])); // 根據"Price"大到小排序
+					BigDecimal price2 = new BigDecimal(o2.get(titleArray[3]));
 					return price2.compareTo(price1);
 				}
 			});
 
-			//新增csv檔到桌面
-			BufferedWriter bufferedwrite = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream("C:\\Users\\Admin\\Desktop\\cars2.csv", true), "UTF-8"));
+			try (// 新增csv檔到桌面
+					BufferedWriter bufferedwrite = new BufferedWriter(new OutputStreamWriter(
+							new FileOutputStream("C:\\Users\\Admin\\Desktop\\cars2.csv", true), "UTF-8"))) {
 
-			bufferedwrite.write(new String(new byte[] { (byte) 0xef, (byte) 0xbb, (byte) 0xbf }));
-			bufferedwrite.append((CharSequence) "MANUFACTURER" + "," + "TYPE" + "," + "MIN.PRICE" + "," + "PRICE");
-			bufferedwrite.newLine();
-			
-			//將排序後的資料輸出至csv檔
-			for (Map carInfo : carList) {
-
-				bufferedwrite.append((CharSequence) carInfo.get("MANUFACTURER") + ",");
-				bufferedwrite.append((CharSequence) carInfo.get("TYPE") + ",");
-				bufferedwrite.append((CharSequence) carInfo.get("MIN.PRICE") + ",");
-				bufferedwrite.append((CharSequence) carInfo.get("PRICE"));
+				// 根據儲存資料屬性名稱的titleArray依序存入資料名稱
+				for (int i = 0; i < titleArray.length; i++) {
+					bufferedwrite.append(titleArray[i] + ",");
+				}
 				bufferedwrite.newLine();
-			}
-			bufferedwrite.flush();
 
-			// 建立一組set儲存廠商資料
-			Set<String> manufactureSet = new TreeSet<String>();
-			manufactureSet.add("Audi");  //用迴圈加進去
-			manufactureSet.add("Acura");
-			manufactureSet.add("Buick");
-			manufactureSet.add("BMW");
-			manufactureSet.add("Chevrolet");
-			manufactureSet.add("Cadillac");
-
-			//首列輸出四屬性資料的標題
-			System.out.printf("%-13s%-8s%-13s%-8s", "MANUFACTURER", "TYPE", "MIN.PRICE", "PRICE");
-			System.out.println();
-			//所有廠商底價以及售價的合計數
-			BigDecimal TotalPrice = new BigDecimal("0.0");
-			BigDecimal TotalMinPrice = new BigDecimal("0.0");
-
-			// 建立大迴圈個別搜尋6個廠商brand
-			for (String brand : manufactureSet) {
-				
-				//各別6個廠商底價以及售價的小計數
-				BigDecimal priceTotal = new BigDecimal("0.0");
-				BigDecimal priceMinTotal = new BigDecimal("0.0");
-
-				// 建立小迴圈搜尋list裡面每輛車的資料。若list裡的map車商對應到大迴圈的廠商brand，則輸出四個屬性的資料，並加總底價以及售價(小計以及合計分別加總)
+				// 將排序後的資料輸出至csv檔
 				for (Map<String, String> carInfo : carList) {
 
-					if (carInfo.get("MANUFACTURER").equals(brand)) {
-						System.out.printf("%-13s%-8s%11s%7s", carInfo.get("MANUFACTURER"), carInfo.get("TYPE"),
-								carInfo.get("MIN.PRICE"), carInfo.get("PRICE"));
-						System.out.println();
-						priceTotal = priceTotal.add(new BigDecimal(carInfo.get("PRICE")));
-						priceMinTotal = priceMinTotal.add(new BigDecimal(carInfo.get("MIN.PRICE")));
-						TotalPrice = TotalPrice.add(new BigDecimal(carInfo.get("PRICE")));
-						TotalMinPrice = TotalMinPrice.add(new BigDecimal(carInfo.get("MIN.PRICE")));
+					// 根據資料種類數量依序將資料輸出至csv檔
+					for (int m = 0; m < titleArray.length; m++) {
+						bufferedwrite.append(carInfo.get(titleArray[m]) + ",");
 					}
+					bufferedwrite.newLine();
+				}
+
+			}
+
+			// 第 6-2 題
+
+	
+			System.out.printf("%-14s%-9s%-13s%-7s", titleArray[0], titleArray[1], titleArray[2], titleArray[3]);
+			System.out.println();
+
+			// 所有廠商底價以及售價的合計數
+
+			ArrayList<BigDecimal> TotalPrice = new ArrayList<>();
+			ArrayList<BigDecimal> TotalMinPrice = new ArrayList<>();
+			
+
+			// 各別6個廠商底價以及售價的小計數
+
+			Map<String, List<Map<String, String>>> groupedCarsMap = carList.stream()
+					.collect(Collectors.groupingBy(car -> car.get(titleArray[0]), TreeMap::new, Collectors.toList()));
+
+			groupedCarsMap.forEach((manufacturer, cars) -> {
+
+				BigDecimal priceTotal = BigDecimal.ZERO;
+				BigDecimal priceMinTotal = BigDecimal.ZERO;
+
+				for (Map<String, String> map : cars) {
+					System.out.printf("%-13s%-8s%11s%7s\n", manufacturer, map.get("Type"), map.get("Min.Price"),
+							map.get("Price"));
+					priceTotal = priceTotal.add(new BigDecimal(map.get("Price")));
+					priceMinTotal = priceMinTotal.add(new BigDecimal(map.get("Min.Price")));
 
 				}
-				System.out.printf("%-13s%18s%7s", "小計", priceMinTotal, priceTotal);
-				System.out.println();
-				//搜尋完一家廠商brand的資料後，印出底價以及售價的小計
-			}
-			System.out.printf("%-13s%18s%7s", "合計", TotalMinPrice, TotalPrice);
-			//搜尋完所有廠商brand的資料後，印出底價以及售價的合計數
+				TotalPrice.add(priceTotal);
+				TotalMinPrice.add(priceMinTotal);
+				System.out.printf("%-13s%18s%7s\n", "小計", priceMinTotal, priceTotal);
+
+			});
+
+			BigDecimal totalminprice = BigDecimal.ZERO;
+			for (BigDecimal minprice : TotalMinPrice)
+				totalminprice = totalminprice.add(minprice);
+
+			BigDecimal totalprice = BigDecimal.ZERO;
+			for (BigDecimal price : TotalPrice)
+				totalprice = totalprice.add(price);
+
+			System.out.printf("%-13s%18s%7s", "合計", totalminprice, totalprice);
+			// 搜尋完所有廠商brand的資料後，印出底價以及售價的合計數
 
 		} catch (Exception e) {
 
@@ -132,3 +142,4 @@ public class practice6 {
 
 	}
 }
+
--- Java/practice7.java
+++ Java/practice7.java
@@ -5,93 +5,100 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class practice7 {
 
-	public static final String UPDATE_CAR_SQL = "update STUDENT.CARS2 set  MIN_PRICE = ?, PRICE = ?  where MANUFACTURER = ? and TYPE = ?";
+	private static final String UPDATE_CAR_SQL = "update STUDENT.CARS2 set  MIN_PRICE = ?, PRICE = ?  where MANUFACTURER = ? and TYPE = ?";
 
-	public static final String INSERT_CARS_SQL = "insert into STUDENT.CARS2 (MANUFACTURER, TYPE, MIN_PRICE, PRICE) values (?, ?, ?, ?)";
+	private static final String INSERT_CARS_SQL = "insert into STUDENT.CARS2 (MANUFACTURER, TYPE, MIN_PRICE, PRICE) values (?, ?, ?, ?)";
 
-	public static final String CONN_URL = "jdbc:oracle:thin:@//localhost:1521/XE";
+	private static final String CONN_URL = "jdbc:oracle:thin:@//localhost:1521/XE";
 
-	public static final String DELETE_CAR_SQL = "delete from STUDENT.CARS2 where MANUFACTURER = ? and TYPE = ?";
+	private static final String DELETE_CAR_SQL = "delete from STUDENT.CARS2 where MANUFACTURER = ? and TYPE = ?";
 
-	public static final String SEARCH_CAR_SQL = "select * from STUDENT.CARS2 where MANUFACTURER = ? and TYPE = ?";
+	private static final String SEARCH_CAR_SQL = "select * from STUDENT.CARS2 where MANUFACTURER = ? and TYPE = ?";
 
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("請選擇以下指令輸入: select、insert、update、delete");
 		String commandOption = scanner.nextLine();
-		
-		//指令集
-		String[] commandArray = { "select", "insert", "update", "delete" };
 
-		//若指令輸入不符規範，提示重新輸入到符合為止
-		while (!Arrays.asList(commandArray).contains(commandOption)) {
+		// 指令集
+		List<String> commandList = new ArrayList<String>();
+		commandList.add("select");
+		commandList.add("insert");
+		commandList.add("update");
+		commandList.add("delete");
+
+		// 若指令輸入不符規範，提示重新輸入到符合為止
+		while (!commandList.contains(commandOption)) {
 			System.out.println("請重新輸入:");
 			commandOption = scanner.nextLine();
 		}
 
-		//根據輸入的指令執行對應的方法
+		System.out.println("請輸入製造商:");
+		String inManufacturer = scanner.nextLine();
+		System.out.println("請輸入類型:");
+		String inType = scanner.nextLine();
+
+		// 根據輸入的指令執行對應的方法
 		switch (commandOption) {
 
 		case "select":
-			doQuery();
+			doQuery(inManufacturer, inType);
 			break;
 
 		case "insert":
-			doInsert();
+			System.out.println("請輸入底價:");
+			String inputPriceMin = scanner.nextLine();
+			System.out.println("請輸入售價:");
+			String inputPrice = scanner.nextLine();
+
+			doInsert(inManufacturer, inType, inputPriceMin, inputPrice);
 			break;
 
 		case "update":
-			doUpdate();
+			System.out.println("請輸入底價:");
+			String updatePriceMin = scanner.nextLine();
+			System.out.println("請輸入售價:");
+			String updatePrice = scanner.nextLine();
+			doUpdate(inManufacturer, inType, updatePriceMin, updatePrice);
 			break;
 
 		case "delete":
-			doDelete();
+			doDelete(inManufacturer, inType);
 			break;
 
 		}
 		scanner.close();
 	}
 
-	//insert方法
-	public static void doInsert() {
+	// insert方法
+	public static void doInsert(String inManufacturer, String inType, String inputPriceMin, String inputPrice) {
 
-		//等待使用者輸入四種屬性的資料
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("請輸入製造商:");
-		String inManufacturer = scanner.nextLine();
-		System.out.println("請輸入類型:");
-		String inType = scanner.nextLine();
-		System.out.println("請輸入底價:");
-		String inputPriceMin = scanner.nextLine();
-		System.out.println("請輸入售價:");
-		String inputPrice = scanner.nextLine();
 
 		try (Connection conn = DriverManager.getConnection(CONN_URL, "student", "student123456");) {
 
-			try {
+			try (PreparedStatement insertValue = conn.prepareStatement(INSERT_CARS_SQL);) {
+
 				conn.setAutoCommit(false);
 
-				PreparedStatement insertCheck = conn.prepareStatement(SEARCH_CAR_SQL);
+				// 若WhetherExist為true，代表有此筆資料，無法輸入，提示錯誤原因
+				if (WhetherExist(inManufacturer, inType)) {
 
-				insertCheck.setString(1, inManufacturer);
-				insertCheck.setString(2, inType);
+					throw new Exception("已有製造商、類型資料。請利用update");
 
-				// ResultSet物件儲存查詢結果
-				ResultSet rs = insertCheck.executeQuery();
-
-				// 若查詢結果製造商以及類型為空值，代表不重複，可以輸入此筆資料
-				if (!rs.isBeforeFirst()) {
+				} else { // 若WhetherExist為false，代表無此筆資料，繼續執行輸入步驟
 
 					// 判斷輸入的底價以及售價是否符合規範，若錯誤則提示直到輸入正確為止
-					while (!Pattern.matches("^\\d*\\.?\\d*", inputPriceMin)
-							|| !Pattern.matches("^\\d*\\.?\\d*", inputPrice)
+					while (!Pattern.matches("^[0-9]*\\.?\\d*", inputPriceMin)
+							|| !Pattern.matches("^[0-9]*\\.?\\d*", inputPrice)
 							|| (Double.parseDouble(inputPriceMin) > Double.parseDouble(inputPrice))) {
 						System.out.println("請重新輸入底價與售價(最多到小數點二位)，且售價需大於底價");
 						System.out.println("請輸入底價:");
@@ -101,8 +108,7 @@ public class practice7 {
 
 					}
 
-					//若查詢結果不重複且價格符合規範，則輸入資料到資料庫並commit
-					PreparedStatement insertValue = conn.prepareStatement(INSERT_CARS_SQL);
+					// 若查詢結果不重複且價格符合規範，則輸入資料到資料庫並commit
 					insertValue.setString(1, inManufacturer);
 					insertValue.setString(2, inType);
 					insertValue.setDouble(3, Double.parseDouble(inputPriceMin));
@@ -112,11 +118,9 @@ public class practice7 {
 					conn.commit();
 
 					System.out.println("新增成功");
-				} else { // 若查詢結果已經有此製造商以及類型，則提示利用update指令
-					throw new Exception("已有製造商、類型資料。請利用update");
 
 				}
-				
+
 			} catch (Exception e) {
 				System.out.println("新增失敗，原因：" + e.getMessage());
 				try {
@@ -128,46 +132,38 @@ public class practice7 {
 
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
-		}finally {
+		} finally {
 			scanner.close();
 		}
-		
+
 	}
 
-	//select方法
-	public static void doQuery() {
+	// select方法
+	public static void doQuery(String inManufacturer, String inType) {
 
 		Scanner scanner = new Scanner(System.in);
 
-		//等待使用者輸入四種屬性的資料
-		System.out.println("請輸入製造商:");
-		String inManufacturer = scanner.nextLine();
-		System.out.println("請輸入類型:");
-		String inType = scanner.nextLine();
+		try (Connection conn = DriverManager.getConnection(CONN_URL, "student", "student123456");
+				PreparedStatement dosearch = conn.prepareStatement(SEARCH_CAR_SQL);) {
 
-		try (Connection conn = DriverManager.getConnection(CONN_URL, "student", "student123456");) {
+			if (WhetherExist(inManufacturer, inType)) { // 若WhetherExist為true，代表有此筆資料，繼續執行步驟
 
-			PreparedStatement dosearch = conn.prepareStatement(SEARCH_CAR_SQL);
-			dosearch.setString(1, inManufacturer);
-			dosearch.setString(2, inType);
+				dosearch.setString(1, inManufacturer);
+				dosearch.setString(2, inType);
 
-			// ResultSet物件儲存查詢結果
-			ResultSet rs = dosearch.executeQuery();
+				ResultSet rs = dosearch.executeQuery();
 
-			// 使用StringBuilder做字串串接
-			StringBuilder sb = new StringBuilder();
-			sb.append("查詢結果： \n");
-
-			// 若查詢結果不為空值，代表有查詢到資料
-			if (rs.isBeforeFirst()) {
+				// 使用StringBuilder做字串串接
+				StringBuilder sb = new StringBuilder();
+				sb.append("查詢結果： \n");
 				while (rs.next()) {
 					sb.append("製造商： ").append(rs.getString("MANUFACTURER")).append("，型號：").append(rs.getString("TYPE"))
 							.append("，售價：").append(rs.getString("PRICE")).append("，底價：")
 							.append(rs.getString("MIN_PRICE"));
 				}
 				System.out.println(sb.toString());
-			} else {
-				// 若查詢結果為空值，代表沒有查詢到資料
+
+			} else {  // 若WhetherExist為false，代表無此筆資料，提示錯誤原因
 				throw new Exception("查無此筆資料");
 			}
 
@@ -175,56 +171,50 @@ public class practice7 {
 			System.out.println("查詢失敗，原因：" + e.getMessage());
 			e.printStackTrace();
 
-		}finally {
+		} finally {
 			scanner.close();
 		}
 	}
 
-	//update方法
-	public static void doUpdate() {
-		
-		//等待使用者輸入四種屬性的資料
+	// update方法
+	public static void doUpdate(String inManufacturer, String inType, String inputPriceMin, String inputPrice) {
+
+		// 等待使用者輸入四種屬性的資料
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("請輸入製造商:");
-		String inManufacturer = scanner.nextLine();
-		System.out.println("請輸入類型:");
-		String inType = scanner.nextLine();
-		System.out.println("請輸入底價:");
-		String inputPriceMin = scanner.nextLine();
-		System.out.println("請輸入售價:");
-		String inputPrice = scanner.nextLine();
 
 		try (Connection conn = DriverManager.getConnection(CONN_URL, "student", "student123456");) {
-			try {
+			try (PreparedStatement selecinsert = conn.prepareStatement(UPDATE_CAR_SQL);) {
 				conn.setAutoCommit(false);
-				PreparedStatement selecinsert = conn.prepareStatement(UPDATE_CAR_SQL);
 
-				// 判斷輸入的底價以及售價是否符合規範，若錯誤則提示直到輸入正確為止
-				while (!Pattern.matches("^\\d*\\.?\\d*", inputPriceMin) || !Pattern.matches("^\\d*\\.?\\d*", inputPrice)
-						|| (Double.parseDouble(inputPriceMin) > Double.parseDouble(inputPrice))) {
-					System.out.println("請重新輸入底價與售價(最多到小數點二位)，且售價需大於底價");
-					System.out.println("請輸入底價:");
-					inputPriceMin = scanner.nextLine();
-					System.out.println("請輸入售價:");
-					inputPrice = scanner.nextLine();
-				}
+				
+				
+				// 若WhetherExist為true，代表有此筆資料，繼續執行步驟
+				if (WhetherExist(inManufacturer, inType)) {
+					
+					// 判斷輸入的底價以及售價是否符合規範，若錯誤則提示直到輸入正確為止
+					while (!Pattern.matches("^[0-9]*\\.?\\d*", inputPriceMin)
+							|| !Pattern.matches("^[0-9]*\\.?\\d*", inputPrice)
+							|| (Double.parseDouble(inputPriceMin) > Double.parseDouble(inputPrice))) {
 
-				//執行update
-				selecinsert.setDouble(1, Double.parseDouble(inputPriceMin));
-				selecinsert.setDouble(2, Double.parseDouble(inputPrice));
-				selecinsert.setString(3, inManufacturer);
-				selecinsert.setString(4, inType);
-				selecinsert.executeUpdate();
+						System.out.println("請重新輸入底價與售價(最多到小數點二位)，且售價需大於底價");
+						System.out.println("請輸入底價:");
+						inputPriceMin = scanner.nextLine();
+						System.out.println("請輸入售價:");
+						inputPrice = scanner.nextLine();
+					}
 
-				// 判斷更新數量updateCount
-				int updateCount = selecinsert.getUpdateCount();
+					// 執行update
+					selecinsert.setDouble(1, Double.parseDouble(inputPriceMin));
+					selecinsert.setDouble(2, Double.parseDouble(inputPrice));
+					selecinsert.setString(3, inManufacturer);
+					selecinsert.setString(4, inType);
+					selecinsert.executeUpdate();
 
-				// 判斷更新數量updateCount，若不為0代表成功更新到資料，並commit
-				if (updateCount != 0) {
+					
 					System.out.println("更新成功");
 					conn.commit();
 
-				} else { // updateCount若為0代表沒更新到資料，此筆資料應該要新增，因此提示需使用insert指令
+				} else { // 若WhetherExist為false，代表無此筆資料，提示錯誤原因
 					throw new Exception("無此製造商以及類別。請利用insert新增資料");
 				}
 
@@ -233,43 +223,38 @@ public class practice7 {
 				try {
 					conn.rollback();
 				} catch (SQLException sqle) {
-//                    sqle.printStackTrace();
+
 					System.out.println("rollback 失敗，原因：" + sqle.getMessage());
 				}
 			}
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
-		}finally {
+		} finally {
 			scanner.close();
 		}
-		
+
 	}
 
-	//delete方法
-	public static void doDelete() {
-
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("請輸入製造商:");
-		String inManufacturer = scanner.nextLine();
-		System.out.println("請輸入類型:");
-		String inType = scanner.nextLine();
+	// delete方法
+	public static void doDelete(String inManufacturer, String inType) {
 
 		try (Connection conn = DriverManager.getConnection(CONN_URL, "student", "student123456");) {
-			try {
+			try (PreparedStatement dodelete = conn.prepareStatement(DELETE_CAR_SQL);) {
 				conn.setAutoCommit(false);
-				PreparedStatement dodelete = conn.prepareStatement(DELETE_CAR_SQL);
-				dodelete.setString(1, inManufacturer);
-				dodelete.setString(2, inType);
 
-				dodelete.executeUpdate();
-				int deleteCount = dodelete.getUpdateCount();
+				
 
-				// 判斷刪除的資料數量deleteCount，若不為0代表成功刪除到資料，並commit
-				if (deleteCount != 0) {
+				// 若WhetherExist為true，代表有此筆資料，繼續執行步驟
+				if (WhetherExist(inManufacturer, inType)) {
+					
+					dodelete.setString(1, inManufacturer);
+					dodelete.setString(2, inType);
+					dodelete.executeUpdate();
+					
 					System.out.println("刪除成功");
 					conn.commit();
-				} else { // deleteCount若為0代表沒刪除到資料，應無此筆資料
+					
+				} else { // 若WhetherExist為false，代表無此筆資料，提示錯誤原因
 					throw new Exception("無此筆資料");
 				}
 
@@ -283,9 +268,32 @@ public class practice7 {
 			}
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
-		}finally {
-			scanner.close();
 		}
 
 	}
+
+	public static boolean WhetherExist(String checkmanufacturer, String checkType) {
+		try (Connection conn = DriverManager.getConnection(CONN_URL, "student", "student123456");
+				PreparedStatement docheck = conn.prepareStatement(SEARCH_CAR_SQL);) {
+
+			docheck.setString(1, checkmanufacturer);
+			docheck.setString(2, checkType);
+
+			ResultSet check = docheck.executeQuery();
+
+			// 若查詢結果不為空值，代表有查詢到資料
+			if (check.isBeforeFirst()) {
+				return true;
+
+			} else {
+				// 若查詢結果為空值，代表沒有查詢到資料
+				throw new Exception();
+			}
+		} catch (Exception e) {
+			return false;
+
+		}
+
+	}
+
 }
